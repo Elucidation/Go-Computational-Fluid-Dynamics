69b0f1f7ec8142b9b7993c22df0627766088b71e
Sam Ansari <elucidation@gmail.com>
2015-03-06 01:52:08 -0800

    moved avg/sum to helper.go

--- day2.go
+++ day2.go
@@ -1,44 +1,17 @@
 package main
 
-import "fmt"
-
-// The main array
-
-func average(arr []float64) float64 {
-	total := 0.0
-	for _, v := range arr {
-		total += v
-	}
-	return total / float64(len(arr))
-}
-
-func sum(arr []float64) float64 {
-	total := 0.0
-	for _, v := range arr {
-		total += v
-	}
-	return total
-}
-
-func step(arr_in []float64, arr_out []float64) {
-	for i := range arr_in {
-		switch i {
-		case 0: // Near edge
-			arr_out[i] = (arr_in[i]*2 + arr_in[i+1]) / 3
-		case len(arr_in) - 1: // Far edge
-			arr_out[i] = (arr_in[i]*2 + arr_in[i-1]) / 3
-		default:
-			arr_out[i] = (arr_in[i-1] + arr_in[i] + arr_in[i+1]) / 3
-		}
-	}
-}
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Printf("Run Test\n")
 
 	const n = 11
 	var (
-		grid     [n]float64
+		// The main array
+		grid [n]float64
+		// Temp array used by step for update
 		grid_tmp [n]float64
 	)
 
---
+++ helper.go
@@ -0,0 +1,30 @@
+package main
+
+func average(arr []float64) float64 {
+	total := 0.0
+	for _, v := range arr {
+		total += v
+	}
+	return total / float64(len(arr))
+}
+
+func sum(arr []float64) float64 {
+	total := 0.0
+	for _, v := range arr {
+		total += v
+	}
+	return total
+}
+
+func step(arr_in []float64, arr_out []float64) {
+	for i := range arr_in {
+		switch i {
+		case 0: // Near edge
+			arr_out[i] = (arr_in[i]*2 + arr_in[i+1]) / 3
+		case len(arr_in) - 1: // Far edge
+			arr_out[i] = (arr_in[i]*2 + arr_in[i-1]) / 3
+		default:
+			arr_out[i] = (arr_in[i-1] + arr_in[i] + arr_in[i+1]) / 3
+		}
+	}
+}
