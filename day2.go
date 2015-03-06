055a57333c780563cd091b88513c27d8be864976
Sam Ansari <elucidation@gmail.com>
2015-03-06 01:11:30 -0800

    prep function

--- day2.go
+++ day2.go
@@ -4,6 +4,10 @@ import "fmt"
 
 // The main array
 
+func average(arr []float64) float64 {
+	panic("Not done")
+}
+
 func main() {
 	fmt.Printf("Run Test\n")
 
@@ -14,11 +18,12 @@ func main() {
 		a[i] = float64(i)
 	}
 
-	total := 0.0
-	for _, v := range a {
-		total += v
-	}
-	avg := total / float64(len(a))
+	// total := 0.0
+	// for _, v := range a {
+	// 	total += v
+	// }
+	// avg := total / float64(len(a))
+	avg := average(a[:])
 
 	fmt.Println(a)
 	fmt.Println("Average: ", avg)
