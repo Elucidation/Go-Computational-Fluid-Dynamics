e8a16c9cbe0e1edc50bb5173059339276f5494d1
Elucidation <elucidation@gmail.com>
2015-05-24 17:42:00 -0700

    [cleanup] step3

---
+++ step3_1D_Diffusion/README.md
@@ -0,0 +1,5 @@
+# 1D Diffusion
+
+http://nbviewer.ipython.org/github/barbagroup/CFDPython/blob/master/lessons/04_Step_3.ipynb
+
+![Example sim](1d_simN1000x2000S.png)
---
+++ step3_1D_Diffusion/diffusion_1d.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os/exec"
+)
+
+type Sim_constants struct {
+	n, steps                    int
+	width, dt, dx, c, sigma     float64
+	totaltime, maxintensity, nu float64
+}
+
+func main() {
+	fmt.Printf("Run Test\n")
+
+	sc := Sim_constants{
+		n:     400, // Number of cells
+		width: 1,   // meters of all cells
+		// steps: 1000, // Number of steps
+		totaltime: 0.1, // seconds
+		// c: float64(0.1),
+		nu:           0.2, // viscosity
+		maxintensity: 2,
+		// sigma:        0.1, // used to determine timestep via courant number
+	}
+	sc.dx = sc.width / float64(sc.n-1)
+	sc.sigma = 1 / sc.maxintensity
+	sc.dt = sc.sigma * math.Pow(sc.dx, 2) / sc.nu
+	sc.steps = int(sc.totaltime / sc.dt)
+	if sc.steps > 4000 {
+		sc.steps = 4000
+	}
+
+	fmt.Println("sigma = ", sc.sigma, ", dx = ", sc.dx, ", dt = ", sc.dt)
+	fmt.Println("Total time = ", float64(sc.steps)*sc.dt, " seconds")
+
+	fmt.Println(sc)
+
+	// The main array
+	grid := make([]float64, sc.n)
+	// Temp array used by step for update
+	grid_tmp := make([]float64, sc.n)
+
+	// Seed grid
+	for i := range grid {
+		grid[i] = 1
+	}
+	fmt.Println(int(.5*sc.width/sc.dx), int(1*sc.width/sc.dx+1))
+	for i := int(0.4 * float64(sc.n)); i < int(0.6*float64(sc.n)+1); i++ {
+		grid[i] = sc.maxintensity
+	}
+	for i := int(0.7 * float64(sc.n)); i < int(0.72*float64(sc.n)+1); i++ {
+		grid[i] = sc.maxintensity
+	}
+
+	maxInitVal := max(grid[:])
+
+	// Draw each 1D grid as a row in a PNG image
+	m := initPNG(sc.steps, sc.n)
+	updatePNG(m, 0, grid[:], maxInitVal)
+
+	for i := 0; i < sc.steps; i++ {
+		step(grid[:], grid_tmp[:], sc)
+		grid, grid_tmp = grid_tmp, grid // Swap
+
+		// fmt.Printf("%.1f\n", grid)
+		updatePNG(m, i+1, grid[:], maxInitVal)
+		fmt.Println("Sum: ", sum(grid[:]), ", Average: ", average(grid[:]))
+	}
+
+	// fmt.Println(grid_tmp)
+	fmt.Println("Sum: ", sum(grid[:]), ", Average: ", average(grid[:]))
+
+	filename := fmt.Sprintf("1d_simN%dx%dS.png", sc.n, sc.steps)
+	writePNG(m, filename)
+
+	// ShowUbuntu(filename)
+	ShowMac(filename)
+}
+
+// Iterates one step of diffusion for the grid
+func step(arr_in []float64, arr_out []float64, sc Sim_constants) {
+	for i := range arr_in {
+		switch i {
+		case 0: // Near edge
+		case len(arr_in) - 1: // Near edge
+			arr_out[i] = arr_in[i] // - arr_in[i]*sc.dt/sc.dx*(arr_in[i])
+		default:
+			// arr_out[i] = arr_in[i] - arr_in[i]*sc.dt/sc.dx*(arr_in[i]-arr_in[i-1])
+			// arr_out[i] = arr_in[i] - arr_in[i]*sc.dt/sc.dx*((arr_in[i]-arr_in[i-1])+(arr_in[i]-arr_in[i+1]))
+			// arr_out[i] = arr_in[i] - arr_in[i]*sc.dt/(sc.dx)*(2*arr_in[i]-arr_in[i+1]-arr_in[i-1])
+			arr_out[i] = arr_in[i] +
+				sc.nu*sc.dt/(math.Pow(sc.dx, 2))*
+					(arr_in[i+1]-2*arr_in[i]+arr_in[i-1])
+		}
+	}
+}
+
+// show  a specified file by Preview.app for OS X(darwin)
+func ShowUbuntu(name string) {
+	command := "eog"
+	cmd := exec.Command(command, name)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// show  a specified file by Preview.app for OS X(darwin)
+func ShowMac(name string) {
+	command := "open"
+	arg1 := "-a"
+	arg2 := "/Applications/Preview.app"
+	cmd := exec.Command(command, arg1, arg2, name)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
